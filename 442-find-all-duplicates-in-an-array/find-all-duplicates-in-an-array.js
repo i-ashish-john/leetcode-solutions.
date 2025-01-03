563912cbe08ee92fc6d5362e9dded9e871c7fad5
Ashish john <135585579+i-ashish-john@users.noreply.github.com>
2025-01-03 11:59:34 +0530

    Time: 55 ms (13.79%) | Memory: 59.9 MB (65.59%) - LeetSync

---
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDuplicates = function(nums) {
+    let result =[]
+    nums.sort((a,b)=>a-b)
+    for(let i =0;i<nums.length;i++){
+       
+            if(nums[i]==nums[i+1]){
+                result.push(nums[i])
+            }
+        
+    }
+    return result
+};
