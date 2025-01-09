0cc970707871746282a89c6d61fa159ac8ef6158
Ashish john <135585579+i-ashish-john@users.noreply.github.com>
2025-01-09 16:47:30 +0530

    Time: 5 ms (41.08%) | Memory: 51.3 MB (81.89%) - LeetSync

--- 3606-minimum-element-after-replacement-with-digit-sum/minimum-element-after-replacement-with-digit-sum.js
+++ 3606-minimum-element-after-replacement-with-digit-sum/minimum-element-after-replacement-with-digit-sum.js
@@ -3,18 +3,7 @@
  * @return {number}
  */
 var minElement = function(nums) {
-    // let minSum = Infinity
-    // for (let num of nums) {
-    //     let sum = 0;
-    //     for (let val of num.toString()) {
-            
-    //         sum += parseInt(val);
-           
-    //     }
-    //         // console.log(sum)
-    //     minSum = Math.min(minSum,sum);
-    // }
-
+    
     for(let i = 0; i < nums.length; i++){
         let value = nums[i].toString()
         .split('')
