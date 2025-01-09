2551bfd180aa80dea8b7c8a9ed70d3d4e400c383
Ashish john <135585579+i-ashish-john@users.noreply.github.com>
2025-01-09 16:32:25 +0530

    Time: 6 ms (31.08%) | Memory: 52.1 MB (32.97%) - LeetSync

--- 3606-minimum-element-after-replacement-with-digit-sum/minimum-element-after-replacement-with-digit-sum.js
+++ 3606-minimum-element-after-replacement-with-digit-sum/minimum-element-after-replacement-with-digit-sum.js
@@ -3,16 +3,27 @@
  * @return {number}
  */
 var minElement = function(nums) {
-    let minSum = Infinity
-    for (let num of nums) {
-        let sum = 0;
-        for (let val of num.toString()) {
+    // let minSum = Infinity
+    // for (let num of nums) {
+    //     let sum = 0;
+    //     for (let val of num.toString()) {
             
-            sum += parseInt(val);
+    //         sum += parseInt(val);
            
-        }
-            // console.log(sum)
-        minSum = Math.min(minSum,sum);
+    //     }
+    //         // console.log(sum)
+    //     minSum = Math.min(minSum,sum);
+    // }
+    let min = 0
+    let prev = 0
+
+    for(let i = 0; i < nums.length; i++){
+        let value = nums[i].toString()
+        .split('')
+        .reduce((acc, curr) => parseInt(curr) + acc, 0);
+        nums[i] = value
     }
-    return minSum;
+
+    return Math.min(...nums)
+
 }
