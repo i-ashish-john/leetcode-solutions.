760f06ea8e0d74e77ed68d45ce6d4d48f246a913
Ashish john <135585579+i-ashish-john@users.noreply.github.com>
2025-02-05 12:50:17 +0530

    Time: 0 ms (100.00%) | Memory: 48.4 MB (93.48%) - LeetSync

---
+++ 1927-maximum-ascending-subarray-sum/maximum-ascending-subarray-sum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxAscendingSum = function(nums) {
+    let maxSum = nums[0],sum = nums[0]
+    for(let i=1;i<nums.length;i++){
+            if(nums[i] > nums[i-1]){
+                sum+=nums[i]
+            }else{
+                maxSum = Math.max(maxSum,sum)
+                sum = nums[i]
+            }
+    }
+    return Math.max(maxSum,sum)
+};
+
