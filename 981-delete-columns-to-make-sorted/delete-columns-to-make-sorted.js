665ee0efbb417734cd86dda1766ed9b06ccfd53f
Ashish john <135585579+i-ashish-john@users.noreply.github.com>
2025-02-06 23:55:05 +0530

    Time: 10 ms (54.97%) | Memory: 53.5 MB (70.86%) - LeetSync

--- 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.js
+++ 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.js
@@ -1,9 +1,11 @@
 /**
  * @param {string[]} strs
- * @return {number}
+ * @retu
+ rn {number}
  */
 var minDeletionSize = function(strs) {
     let count = 0;
+    console.log(strs[1])
     for (let j = 0; j < strs[0].length; j++) {
         for (let i = 1; i < strs.length; i++) { 
             if (strs[i][j] < strs[i - 1][j]) {
