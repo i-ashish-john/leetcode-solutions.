0b22c5caf2c64715ff0cd0e801a385c47f915b6b
Ashish john <135585579+i-ashish-john@users.noreply.github.com>
2025-02-11 15:13:24 +0530

    Time: 0 ms (100.00%) | Memory: 49.3 MB (13.49%) - LeetSync

--- 1406-subtract-the-product-and-sum-of-digits-of-an-integer/subtract-the-product-and-sum-of-digits-of-an-integer.js
+++ 1406-subtract-the-product-and-sum-of-digits-of-an-integer/subtract-the-product-and-sum-of-digits-of-an-integer.js
@@ -5,11 +5,12 @@
 var subtractProductAndSum = function(n) {
     let product = 1
     let sum = 0 
-    while(n !== 0){
-        let num = n % 10
-        product *= num
-        sum += num
-        n = Math.floor(n / 10)
-    }
-    return product - sum
+ 
+  while(n!==0){
+        let num = n%10
+         product*= num
+         sum+= num
+         n = Math.floor(n/10)
+  }
+  return product-sum
 };
