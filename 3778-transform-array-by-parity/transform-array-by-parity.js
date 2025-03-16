4c6743a2896973851eb6ed065ec746a6233f670f
Ashish john <135585579+i-ashish-john@users.noreply.github.com>
2025-03-16 17:25:55 +0530

    Time: 4 ms (13.60%) | Memory: 57.3 MB (52.17%) - LeetSync

--- 3778-transform-array-by-parity/transform-array-by-parity.js
+++ 3778-transform-array-by-parity/transform-array-by-parity.js
@@ -3,13 +3,14 @@
  * @return {number[]}
  */
 var transformArray = function(nums) {
-    for(let i =0;i<nums.length;i++){
-        if(nums[i]%2==0){
-            nums[i] = 0
-        }else{
-            nums[i] =1
-        }
-    }
+   
+const result = nums.map(item => {
+  return item % 2 === 0 ? 0 : 1;
+});
 
-    return nums.sort((a,b)=>a-b)
+   return result.sort((a,b)=>a-b)
 };
+
+
+
+
