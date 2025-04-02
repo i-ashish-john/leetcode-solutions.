651d024aac44910dbfd4b15261f6885d6ab9d7fd
Ashish john <135585579+i-ashish-john@users.noreply.github.com>
2025-04-02 23:45:03 +0530

    Time: 3 ms (29.73%) | Memory: 55.6 MB (21.62%) - LeetSync

---
+++ 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumTripletValue = function(nums) {
+    let maxVal = 0;
+    for(let i = 0; i < nums.length - 2; i++){
+        for(let j = i + 1; j < nums.length - 1; j++){
+            for(let k = j + 1; k < nums.length; k++){
+                const value = (nums[i] - nums[j]) * nums[k];
+                maxVal = Math.max(value,maxVal)
+            }
+        }
+    }
+    return maxVal
+};
