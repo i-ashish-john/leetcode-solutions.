977f822712fb7a1ae5baf419bad69af296886201
Ashish john <135585579+i-ashish-john@users.noreply.github.com>
2025-04-06 00:13:51 +0530

    Time: 0 ms (100.00%) | Memory: 53.6 MB (61.63%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.js
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.js
@@ -3,9 +3,9 @@
  * @return {number}
  */
 var subsetXORSum = function(nums) {
-  let a = 0;
+  let val = 0;
   for (let n of nums) {
-    a |= n;
+    val |= n;
   }
-  return a * (1 << (nums.length - 1));
+  return val * (1 << (nums.length - 1));
 };
