cb5d4e88a1e9ab72d2e49ce3e2d733703014693a
Ashish john <135585579+i-ashish-john@users.noreply.github.com>
2025-06-11 09:11:44 +0530

    Time: 4 ms (64.29%) | Memory: 54.9 MB (78.57%) - LeetSync

--- 1363-greatest-english-letter-in-upper-and-lower-case/greatest-english-letter-in-upper-and-lower-case.js
+++ 1363-greatest-english-letter-in-upper-and-lower-case/greatest-english-letter-in-upper-and-lower-case.js
@@ -9,7 +9,7 @@ var greatestLetter = function(s) {
     for (let i = 0; i < 26; i++) {
         let upper = String.fromCharCode(65 + i); 
         let lower = String.fromCharCode(97 + i); 
-
+        // console.log('upper--->'+upper, 'lower---->'+lower)
         if (set.has(upper) && set.has(lower)) {
             result = upper; 
         }
