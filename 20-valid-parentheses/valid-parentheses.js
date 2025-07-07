44b7bcb32757e2f1e66842e5f912c05507b845a8
Ashish john <135585579+i-ashish-john@users.noreply.github.com>
2025-07-07 12:57:16 +0530

    Time: 1 ms (85.84%) | Memory: 55.5 MB (57.01%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.js
+++ 20-valid-parentheses/valid-parentheses.js
@@ -4,10 +4,15 @@
  */
 var isValid = function(s) {
     let obj={'(':')','{':'}','[':']'}
-    let stack=[]
-    for(let char of s){
-        if(obj[char]) stack.push(char)
-        else if(obj[stack.pop()]!==char) return false
+
+     let stack =[]
+
+    for(let char of s){ 
+        if(obj[char]){
+            stack.push(char)
+        }else if (obj[stack.pop()]!== char){
+            return false
+        }
     }
-    return stack.length==0
+  return stack.length==0
 };
