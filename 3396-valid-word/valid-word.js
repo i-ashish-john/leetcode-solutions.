51bbe1be7e24b340573be35844268f02b0363f9d
Ashish john <135585579+i-ashish-john@users.noreply.github.com>
2025-07-15 12:08:58 +0530

    Time: 1 ms (55.10%) | Memory: 54.5 MB (63.27%) - LeetSync

---
+++ 3396-valid-word/valid-word.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+var isValid = function(word) {
+  return word.length >= 3 &&
+/[aeiouAEIOU]/.test(word) && /[b-df-hj-np-tv-z]/i.test(word) && /^[a-zA-Z0-9]+$/.test(word)
+    
+};
