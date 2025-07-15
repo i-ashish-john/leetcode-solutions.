822b9845e47fd5e93d51808c14ba9657da86c7c8
Ashish john <135585579+i-ashish-john@users.noreply.github.com>
2025-07-15 12:10:07 +0530

    Time: 0 ms (100.00%) | Memory: 53.8 MB (81.63%) - LeetSync

--- 3396-valid-word/valid-word.js
+++ 3396-valid-word/valid-word.js
@@ -3,7 +3,9 @@
  * @return {boolean}
  */
 var isValid = function(word) {
-  return word.length >= 3 &&
-/[aeiouAEIOU]/.test(word) && /[b-df-hj-np-tv-z]/i.test(word) && /^[a-zA-Z0-9]+$/.test(word)
+  return word.length >= 3        &&
+  /[aeiouAEIOU]/.test(word)      &&
+  /[b-df-hj-np-tv-z]/i.test(word)&&
+  /^[a-zA-Z0-9]+$/.test(word)
     
 };
