84da9162b413d53175829915665ef04b23cb48e5
Ashish john <135585579+i-ashish-john@users.noreply.github.com>
2025-08-06 20:36:04 +0530

    Time: 0 ms (100.00%) | Memory: 58.3 MB (13.70%) - LeetSync

---
+++ 777-toeplitz-matrix/toeplitz-matrix.ts
@@ -0,0 +1,14 @@
+function isToeplitzMatrix(matrix: number[][]): boolean {
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+
+  for (let i = 1; i < rows; i++) {
+    for (let j = 1; j < cols; j++) {
+      if (matrix[i][j] !== matrix[i - 1][j - 1]) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
