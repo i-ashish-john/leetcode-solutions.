eafebc689dbbd672e7239d3bee1a75393a1147e4
Ashish john <135585579+i-ashish-john@users.noreply.github.com>
2025-08-24 14:36:08 +0530

    Time: 2 ms (47.27%) | Memory: 54.1 MB (38.18%) - LeetSync

--- 1626-can-make-arithmetic-progression-from-sequence/can-make-arithmetic-progression-from-sequence.js
+++ 1626-can-make-arithmetic-progression-from-sequence/can-make-arithmetic-progression-from-sequence.js
@@ -3,10 +3,9 @@
  * @return {boolean}
  */
 var canMakeArithmeticProgression = function(arr) {
-    if(arr.length<2) return false
+    // if(arr.length<2) return false
     arr.sort((a,b)=>b-a)
     let s = arr[0]-arr[1]
-    console.log(s)
     for(let i = 1;i<arr.length;i++){
         if(arr[i-1] - arr[i] !== s ){
             return false
