45907300c8442b9de848794ae9d5eaaaf3a8e18f
Ashish john <135585579+i-ashish-john@users.noreply.github.com>
2025-10-16 15:08:52 +0530

    Time: 1 ms (83.84%) | Memory: 64.9 MB (22.22%) - LeetSync

--- 3973-flip-square-submatrix-vertically/flip-square-submatrix-vertically.js
+++ 3973-flip-square-submatrix-vertically/flip-square-submatrix-vertically.js
@@ -6,10 +6,13 @@
  * @return {number[][]}
  */
 var reverseSubmatrix = function(grid, x, y, k) {
-        for (let j = y; j < y + k; j++) {
-        for (let i = 0; i < k / 2; i++) {
-            const temp = grid[x + i][j];
-            grid[x + i][j] = grid[x + k - 1 - i][j];
+        for (let j = y; j < y+k; j++) {
+        for (let i = 0; i < k/2; i++) {
+
+            const temp = grid[x+i][j];
+
+            grid[x+i][j] = grid[x + k-1-i][j];
+
             grid[x + k - 1 - i][j] = temp;
         }
     }
