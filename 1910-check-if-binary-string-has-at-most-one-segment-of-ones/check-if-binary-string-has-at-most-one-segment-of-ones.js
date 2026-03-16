cfc5739b5f4a7ac10bff44ede89a6c330693399a
Ashish john <135585579+i-ashish-john@users.noreply.github.com>
2026-03-16 17:15:57 +0530

    Time: 0 ms (100.00%) | Memory: 54.3 MB (12.65%) - LeetSync

---
+++ 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var checkOnesSegment = function(s) {
+    let hasSeenZero = false;
+    
+    for (let char of s) {
+        if (char === '0') {
+            hasSeenZero = true;
+        } else if (hasSeenZero) {
+            return false;
+        }
+    }
+    
+    return true;
+};
